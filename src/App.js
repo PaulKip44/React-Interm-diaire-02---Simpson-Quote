7d3223b96d13ddc4d45646d95ca1b796230a933b
Paul SANCHEZ <paul.sanchez.44@gmail.com>
2022-04-11 12:20:17 +0200

    final version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import QuoteCard from "./QuoteCard";
+import logo from "./logo.svg";
+import "./App.css";
+
+const sampleQuote = {
+  quote:
+    'For once maybe someone will call me "sir" without adding, "You\'re making a scene."',
+  character: "Homer Simpson",
+  image:
+    "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+  characterDirection: "Right",
+};
 
 function App() {
+  const [quote, setQuote] = React.useState(sampleQuote);
+
+  const getQuote = () => {
+    // Send the request
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      // Extract the DATA from the received response
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => {
+        console.log(data);
+        setQuote(data[0]);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuoteCard quote={quote} />
+      <button type="button" onClick={getQuote}>
+        Get a new quote !
+      </button>
     </div>
   );
 }
